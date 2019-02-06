4ac156b39f48ec94d9508176a2643dc8f4ef342f
matt-smrke <matthew.smrke@empire.ca>
2019-02-06 18:15:47 -0500

    update intro text

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -5,8 +5,10 @@ const Intro = (props) => {
   return (
     <div className="vertical-center">
       <div className="container text-center">
-        <h2>Welcome to the BAIF task!</h2>
-        <p>BAIF intro and instructions placeholder.</p>
+        <h2>Welcome to the change detection task!</h2>
+        <p>You will see a circle pulse (grow and then shrink) twice.</p>
+        <p>Your task is to determine if the second pulse is faster or slower than the first pulse.</p>
+        <p>Please press any key to continue...</p>
         <button
           className="btn btn-primary btn-lg"
           onClick={props.finishStep}
